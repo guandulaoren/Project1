957f4cedc681c2b27537ddd39f03b16486904a19
gepeng <mimashi123>
2021-11-17 17:37:53 +0800

    增加单层循环的遍历方法

--- Project1/main.c
+++ Project1/main.c
@@ -99,10 +99,58 @@ void lastFind(Node* tree) {
 	}
 }
 
+void middleFind2(Node* tree) {
+	int stack[MAX_SIZE];
+	int top = -1;
+	stack[++top] = tree;
+	Node* p = (*tree).lChild;
+	while (top != -1) {
+		if (p != NULL) {
+			stack[++top] = p;
+			p = (*p).lChild;
+		} else {
+			Node* node = stack[top--];
+			printf("%d\n", (*node).data);
+			Node* rp = (*node).rChild;
+			if (rp != NULL) {
+				stack[++top] = rp;
+				p = (*rp).lChild;
+			}
+		}
+	}
+}
+
+void lastFind2(Node* tree) {
+	int stack[MAX_SIZE];
+	int top = -1;
+	stack[++top] = tree;
+	// 当前要遍历的树
+	Node* p = (*tree).lChild;
+	// 上一个已遍历过的树
+	Node* prep = NULL;
+	while (top != -1) {
+		if (p != NULL) {
+			stack[++top] = p;
+			p = (*p).lChild;
+		} else {
+			Node* node = stack[top];
+			Node* rp = (*node).rChild;
+			if (rp == NULL || rp == prep) {
+				Node* node = stack[top--];
+				printf("%d\n", (*node).data);
+				prep = node;
+			} else {
+				stack[++top] = rp;
+				p = (*rp).lChild;
+			}
+		}
+	}
+}
+
 int main() {
 	Node* tree = create();
 	//firstFind(tree);
-	//middleFind(tree);
-	lastFind(tree);
+	middleFind2(tree);
+	//lastFind2(tree);
 	return 0;
 }
